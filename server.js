78114c5b60da81abee35da8b4d5116648ac8e9e8
gustavo-it <gustavo_castrosilva@yahoo.com>
2024-10-10 09:32:45 -0300

    structuring the project

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
-import helloWorld from './app';
+import app from './app';
 
-helloWorld();
+const port = 3000;
+app.listen(port, () => console.log(`listen port ${port}`), console.log(`http://localhost:${port}`));
